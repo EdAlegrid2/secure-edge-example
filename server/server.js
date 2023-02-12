c05ae3387145c8f86225269f715b39f822018cf2
EdAlegrid2 <121866690+EdAlegrid2@users.noreply.github.com>
2023-02-12 09:53:49 -0500

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,34 +1,80 @@
-//const { edge, createDevice } = require('m2m')
+'use strict'
+
+const { scryptSync, createCipheriv, createDecipheriv, randomBytes } = require('node:crypto')
+const { Buffer } = require('node:buffer')
 const m2m = require('m2m')
 
-function tempSource(){
-  return 20 + Math.floor(Math.random() * 10)
+let key = null 
+
+function voltageSource(){
+  return 50 + Math.floor(Math.random() * 10)
 }
 
-// edge server using local private tcp connection
-let port = 8126, host = '192.168.0.113'
-
-// m2m server using internet connection
-let device = m2m.createDevice(100)
+// m2m server
+let device = new m2m.Device(200)
 
 m2m.connect('https://dev.node-m2m.com', () => {
-  device.publish('m2m-temperature', (data) => {
-    let ts = tempSource()
-    data.send(ts)
-  })
 
-  m2m.edge.createServer(port, (server) => {
-    console.log('edge server 1 :', host, port)
-
-    server.publish('edge-temp', (data) => {
-      let ts = tempSource()
-      //console.log('ts', ts)
-      data.interval = 10000 
-      data.send(JSON.stringify({value:ts}))
+    device.dataSource('e-key', (data)=>{
+        if(!data.payload){
+            data.send('invalid key')
+        }
+        //let dec = JSON.parse(data.payload) // no need for m2m, parsing is done internally
+        let dec = data.payload 
+        key = Buffer.from(dec.key, 'hex')
+        data.send('key recvd')
     })
 
-    server.on('error', (err) => {
-      console.log('server error', err.message);  
+    const algorithm = 'aes-256-cbc'
+
+    //const Iv = Buffer.alloc(16, 1) // fixed value iv
+    const Iv = randomBytes(16)      // random iv
+
+    // edge server
+    m2m.edge.createServer(8127, '127.0.0.1', (server) => {
+
+        server.dataSource('dec-data', (data) => {
+            if(!data.payload){
+                data.send('invalid payload')
+            }
+
+            let dec = JSON.parse(data.payload)
+            let encrypted = dec.encrypted
+            let iv = Buffer.from(dec.iv, 'hex')
+      
+            try{
+                const decipher = createDecipheriv(algorithm, key, iv)
+                let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+                decrypted += decipher.final('utf8');
+                console.log(decrypted);
+                // send back decrypted data, for test only
+                data.send(decrypted)
+            }
+            catch(e){
+                console.log('dec-data decrypt error:', e.message);
+            }
+        
+        })
+
+        server.pub('enc-data', (data) => {
+                
+            let vs = voltageSource().toString()
+            //let vs = '10' // constant voltage 
+            console.log(vs)
+
+            if(key){ 
+                try{  
+                    const cipher = createCipheriv(algorithm, key, Iv)
+                    let encrypted = cipher.update(vs, 'utf8', 'hex')
+                    encrypted += cipher.final('hex')
+
+                    let epl = JSON.stringify({ encrypted:encrypted, iv:Iv.toString('hex') })
+                    data.send(epl)
+                }
+                catch(e){
+                    console.log('enc-data encrypt error:', e.message);
+                }
+            }
+        })
     })
-  })
 })
